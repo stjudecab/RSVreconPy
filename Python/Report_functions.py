df78ee5063bcf3d2e3bb6e43a2a51e7bc1cb3b8a
Lei Li <leilioxford@gmail.com>
2024-03-12 09:56:55 -0500

    Update Report_functions.py

--- Python/Report_functions.py
+++ Python/Report_functions.py
@@ -8,7 +8,7 @@ from reportlab.lib.styles import getSampleStyleSheet, TA_LEFT
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Flowable, Table, Image, TableStyle
 from reportlab.lib.colors import black
 from reportlab.lib import colors
-
+from reportlab.lib.units import inch
 
 class Line(Flowable):
     """Line flowable --- draws a line in a flowable"""
@@ -50,6 +50,15 @@ def generate_pdf_report(csv_file, working_folder):
     
     line_width = 440
 
+    # make a temp folder under working folder
+    temp_folder = os.path.join(working_folder, 'Temp')
+    if not os.path.exists(temp_folder):
+        # Create the folder
+        os.makedirs(temp_folder)
+        print(f"Folder '{temp_folder}' created successfully for temporary files.")
+    else:
+        print(f"Using existing folder '{temp_folder}' for temporary files.")
+
     # resource path and file path
     file_path = os.path.dirname(os.path.realpath(__file__))
     Logo = os.path.join(file_path, 'Resource','CAB_Hz.png')
@@ -89,30 +98,72 @@ def generate_pdf_report(csv_file, working_folder):
     elements.append(spacer)
 
     # load table for summary section
-    df = pd.read_csv(csv_file, skiprows=2)
+    df = pd.read_csv(csv_file, header=0)
     # Format the column as percentages
     df.iloc[:,7] = df.iloc[:,7].apply(lambda x: '{:.2%}'.format(x/100))
-    df.iloc[:,8] = df.iloc[:,8].apply(lambda x: '{:.2%}'.format(x/100))
-    df.iloc[:,12] = df.iloc[:,12].apply(lambda x: '{:.2%}'.format(x/100))
+    #df.iloc[:,8] = df.iloc[:,8].apply(lambda x: '{:.2%}'.format(x/100))
+    #df.iloc[:,12] = df.iloc[:,12].apply(lambda x: '{:.2%}'.format(x/100))
 
     df = df.iloc[:, [0, 7, 8, 12,16]]
-    df.columns = ['Sample name', 'Pass QC', 'Mapping SubTypeA', 'Mapping SubTypeB', 'Subtype']
     data = df.values.tolist()
-    data.insert(0, df.columns.tolist())
+    
+    # make figure for mapping rate for each sample
+    for row in range(0, len(data)):
+        cur_sample_name = data[row][0]
+        cur_sample_mapA = data[row][2]
+        cur_sample_mapB = data[row][3]
+
+        # data
+        x = ["Subtype A","Subtype B"]
+        y = [cur_sample_mapA, cur_sample_mapB]
+        mapping_colors = ['blue', 'green']
+
+        # plot
+        plt.figure(figsize=(6.5, 0.5))
+        plt.barh(x, y, color=mapping_colors)
+        plt.xlim(0, 100)
+
+        # hide tick, label and borders
+        plt.xlabel('')
+        plt.ylabel('')
+        plt.title('')
+        plt.xticks([])
+        plt.gca().spines['top'].set_visible(False)
+        plt.gca().spines['right'].set_visible(False)
+        plt.gca().spines['bottom'].set_visible(False)
+        plt.gca().spines['left'].set_visible(False)
+
+        # set text label
+        for i, v in enumerate(y):
+            plt.text(v + 1, i, str(v) + '%', color='black', va='center')
+        
+        # save fig
+        png_file = os.path.join(temp_folder, cur_sample_name + '_mapping_figure.png')
+        plt.savefig(png_file, dpi = 300)
+        plt.close()
+
+        cur_sample_png = Image(png_file, width=4.5*inch, height=0.4*inch)
+        data[row] = [cur_sample_name, data[row][1], cur_sample_png, data[row][4]]
+
+    df_columns = ['Sample name', 'Pass QC', 'Mapping rate', 'Subtype']
+    data.insert(0, df_columns)
     table = Table(data)
-    table._argW = [80,60,100,100,80]
+    #table._argW = [100,60,150,80]
 
     # Create a TableStyle and add it to the table
     style = TableStyle([
         ('BACKGROUND', (0, 0), (-1, 0), colors.grey),  # set background color for the first row to grey
         ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),  # set text color for the first row to white
 
+        ('LINEBEFORE', (0, 0), (-1, -1), 0, (1, 1, 1)),  # Hide left border
+        ('LINEAFTER', (0, 0), (-1, -1), 0, (1, 1, 1)),   # Hide right border
+
         ('ALIGN', (0, 0), (-1, -1), 'CENTER'),  # align all cells to the center
         ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),  # set font for the first row to Helvetica-Bold
         ('FONTSIZE', (0, 0), (-1, 0), 9),  # set font size for the first row to 8
 
         ('BOTTOMPADDING', (0, 0), (-1, 0), 12),  # add more space below the text of the first row
-        ('BACKGROUND', (0, 1), (-1, -1), colors.beige),  # set background color for the other rows to beige
+        ('BACKGROUND', (0, 1), (-1, -1), colors.white),  # set background color for the other rows to beige
         ('GRID', (0,0), (-1,-1), 1, colors.black)  # set grid color to black and line width to 1
     ])
 
@@ -122,10 +173,10 @@ def generate_pdf_report(csv_file, working_folder):
             value = percentage_to_number(data[row][col])
             style.add('BACKGROUND', (col,row), (col,row), color_gradient(value))
 
-    for row in range(1, len(data)):
-        for col in [2,3]:
-            value = percentage_to_number(data[row][col])
-            style.add('BACKGROUND', (col,row), (col,row), color_gradient_blue(value))
+    #for row in range(1, len(data)):
+    #    for col in [2,3]:
+    #        value = percentage_to_number(data[row][col])
+    #        style.add('BACKGROUND', (col,row), (col,row), color_gradient_blue(value))
 
     table.setStyle(style)
 
@@ -142,12 +193,13 @@ def generate_pdf_report(csv_file, working_folder):
     subtitle_summary_section = Paragraph("Details", styles['Heading2'])
     elements.append(subtitle_summary_section)
 
-    df = pd.read_csv(csv_file, skiprows = 1, header=0, index_col=0)
+    df = pd.read_csv(csv_file, skiprows = 0, header=0, index_col=0)
     Sample_folders = [f for f in os.listdir(working_folder) if os.path.isdir(os.path.join(working_folder, f))]
     for cur_folder in Sample_folders:
         if os.path.isfile(cur_folder):
             continue
-
+        if cur_folder == "Temp":
+            continue
         #print(cur_folder + "\n")
 
         subtitle_cur_sample = Paragraph("Sample: " + cur_folder, styles['Heading3'])
@@ -245,8 +297,8 @@ def generate_pdf_report(csv_file, working_folder):
 
         # coverage figure
         genome_colors = ['red', 'yellow', 'green', 'cyan', 'blue', 'magenta', '#CCCC00', '#800080', '#D2B48C', '#8B4513', '#ADD8E6'] 
-        fig = plt.figure(figsize=(12, 6))
-        gs = gridspec.GridSpec(3, 1, height_ratios=[6, 1.3, 0.5], hspace=0.35)
+        fig = plt.figure(figsize=(12, 4))
+        gs = gridspec.GridSpec(2, 1, height_ratios=[4, 0.5], hspace=0.05)
         font_axis = {'family': 'sans-serif', 'color': 'black', 'weight': 'bold', 'size': 12}
 
         gs1 = gridspec.GridSpecFromSubplotSpec(1, 1, subplot_spec=gs[0])
@@ -257,7 +309,7 @@ def generate_pdf_report(csv_file, working_folder):
         ax_cov.fill_between(cov_pos, cov, color = 'gray')
         ax_cov.set_xticks([])  # Set the positions for the ticks
 
-        gs2 = gridspec.GridSpecFromSubplotSpec(3, 1, subplot_spec=gs[1], hspace=0.1)
+        gs2 = gridspec.GridSpecFromSubplotSpec(2, 1, subplot_spec=gs[1], hspace=0.05)
         ax_genome = plt.subplot(gs2[0], sharex=ax_cov)
         for interval, color in zip(genome_intervals, genome_colors):
             ax_genome.broken_barh([interval], (10, 9), facecolors=color, edgecolors=color)
@@ -265,7 +317,8 @@ def generate_pdf_report(csv_file, working_folder):
         ax_genome.set_xticklabels(gene_names)  # Set the labels for the ticks
         # Remove the y-ticks
         ax_genome.set_yticks([])
-        ax_genome.set_ylabel('Genes', fontdict=font_axis)
+        ax_genome.set_ylabel('Genes', rotation=0, fontdict=font_axis, labelpad=30)
+        ax_genome.yaxis.set_label_coords(0,-0.1)
 
         # Remove the border
         ax_genome.spines['top'].set_visible(False)
@@ -273,10 +326,11 @@ def generate_pdf_report(csv_file, working_folder):
         ax_genome.spines['bottom'].set_visible(False)
         ax_genome.spines['left'].set_visible(False)
 
-        plt.savefig(cur_folder + '_fig.png', dpi = 300)
+        png_file = os.path.join(temp_folder, cur_folder + '_coverage_figure.png')
+        plt.savefig(png_file, dpi = 300)
         plt.close()
 
-        img_cov = Image(cur_folder + '_fig.png', width=600, height=300)
+        img_cov = Image(png_file, width=600, height=200)
         elements.append(img_cov)
 
     # build the doc
